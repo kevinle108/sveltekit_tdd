d2aa25acbec24afcad58f5c3dd7f08e07518b086
Kevin Le <kevinle108@gmail.com>
2022-01-19 22:17:47 -0500

    first commit

---
+++ src/__tests__/Welcome.test.ts
@@ -0,0 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ import '@testing-library/jest-dom'
+ import Index  from '../routes/index.svelte';
+ import { render } from '@testing-library/svelte';
+
+ describe("Test if Jest is working", () => {
+  test('Welcome', () => {
+    const { getByText } = render(Index);
+    expect(getByText('Welcome to SvelteKit')).toBeInTheDocument();
+  });
+
+  test('Welcome', () => {
+    const { getByText } = render(Index);
+    expect(getByText('Welcome to SvelteKit')).toBeInTheDocument();
+  });
+});
+
+describe("Test if Jest is working", () => {
+  test('Welcome', () => {
+    const { getByText } = render(Index);
+    expect(getByText('Welcome to SvelteKit')).toBeInTheDocument();
+  });
+
+  test('Welcome', () => {
+    const { getByText } = render(Index);
+    expect(getByText('Welcome to SvelteKit')).toBeInTheDocument();
+  });
+});
