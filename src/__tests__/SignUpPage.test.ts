d567ec282b3b3d2208e11c652ba80ac141b9072d
Kevin Le <kevinle108@gmail.com>
2022-01-20 14:48:53 -0500

    wrote more tests

--- src/__tests__/SignUpPage.test.ts
+++ src/__tests__/SignUpPage.test.ts
@@ -3,14 +3,60 @@
  */
 
 import SignUpPage from '$lib/SignUpPage.svelte';
-import { render, screen, fireEvent } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 import '@testing-library/jest-dom'
 
 describe('Sign Up Page', () => {
-  it('has Sign Up header', () => {
-    render(SignUpPage);
-    const header = screen.getByRole('heading', { name: 'Sign Up' });
-    expect(header).toBeInTheDocument();
-  });
+  describe('Layout', () => {
+    it('has Sign Up header', () => {
+      render(SignUpPage);
+      const header = screen.getByRole('heading', { name: 'Sign Up' });
+      expect(header).toBeInTheDocument();
+    });
+
+    it('has username input', () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText('Username');
+      expect(input).toBeInTheDocument();
+    });
+
+    it('has email input', () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText('Email');
+      expect(input).toBeInTheDocument();
+    });
+    
+    it('has password input', () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText('Password');
+      expect(input).toBeInTheDocument();
+    });
+
+    it('has password type for password input', () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText('Password') as HTMLInputElement;
+      expect(input.type).toBe('password')
+    });
+    
+    it('has password repeat input', () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText('Password Repeat');
+      expect(input).toBeInTheDocument();
+    });
+
+    it('has password type for password repeat input', () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText('Password Repeat') as HTMLInputElement;
+      expect(input.type).toBe('password')
+    });
+
+    it('has a sign up button', () => {
+      render(SignUpPage);
+      const button = screen.getByRole('button', {name: 'Sign Up'});
+      expect(button).toBeInTheDocument();
+    });
+
+
+  });  
 });
 
--- src/lib/SignUpPage.svelte
+++ src/lib/SignUpPage.svelte
@@ -3,6 +3,19 @@
 </script>
 
 <h1>Sign Up</h1>
+<label for="username">Username</label>
+<input id="username"/>
+
+<label for="email">Email</label>
+<input id="email"/>
+
+<label for="password">Password</label>
+<input id="password" type="password"/>
+
+<label for="password-repeat">Password Repeat</label>
+<input id="password-repeat" type="password"/>
+
+<button>Sign Up</button>
 
 <style>
 
