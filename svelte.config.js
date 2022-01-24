75676be314bcb29a2db98e916ced3f90852d9dd3
Kevin Le <kevinle108@gmail.com>
2022-01-23 21:23:13 -0500

    successful re-route to standalone backend

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,15 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+		vite: {
+			server: {
+							proxy: {
+											'/api/1.0/users': 'http://localhost:8080'
+							}
+			}
+		}
 	}
 };
 
